27e6ebb387d1556586272ff9cddce1755467cc62
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-05-08 07:56:36 +0800

    [IMP] 回文子串 Manacher 算法

--- 647.回文子串/solution.py
+++ 647.回文子串/solution.py
@@ -69,4 +69,32 @@ class Solution:
 
     def countSubstringManacher(self, s: str) -> int:
         """ Manacher 算法 """
-        pass
+        t = '$#' + '#'.join(s)
+        n = len(t)
+        t += '#!'  # 加入 $ 和 ! 是为了中心扩展时避免越界
+
+        # f[i] 为改造后的 t 的第 i 个字符为回文中心的最大回文半径（包含中心点）
+        # 由此， 第 i 个字符的最大回文串长度 L = f(i) - 1
+        f = [0] * n
+        # im 当前最长回文串的中心点
+        # rm 当前最长回文串右端点
+        im, rm, ans = 0, 0, 0
+
+        for i in range(1, n):
+            # 初始化 f[i]
+            if i <= rm:
+                # 2 * im - i 是 i 关于 im 对称的位置，f[2 * im - i] 就是对称位置的最大回文半径
+                # rm - i + 1 是右端点距离 i 的长度，这时 i 的最长回文串长度 f[i] 有可能大于 rm - i + 1
+                # 因为不确定长度，所以取最小值，保证以 i 为中心的回文串在当前最大回文串内
+                f[i] = min(rm - i + 1, f[2 * im - i])
+            # 中心扩展
+            # i + f[i] 是以 i 为中心的回文半径的下一个位置，特别地当 f[i] = 0，就是单字符
+            while t[i + f[i]] == t[i - f[i]]:
+                f[i] += 1
+            # 以 i 为中心点的最右端位是 i + f[i] - 1
+            # 更新最长回文串，包括中心点和最右端点
+            if i + f[i] - 1 > rm:
+                im, rm = i, i + f[i] - 1
+            # 统计回文子串个数
+            ans += f[i] // 2
+        return ans
