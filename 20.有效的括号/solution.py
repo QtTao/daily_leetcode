95c6bfbe8cd05dac3b7291dc8ee1eb3290ba13e6
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-05-10 08:01:42 +0800

    [ADD] 2021-05-04 每日一题 有效的括号

---
+++ 20.有效的括号/solution.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# author      : Tao Qitian
+# email       : taoqt@mail2.sysu.edu.cn
+# datetime    : 2021/5/10 00:55
+# filename    : solution.py
+# description : LC 20 有效的括号
