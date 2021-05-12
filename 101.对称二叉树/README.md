83228634a061e8dae88f62513a3da27a48cf1017
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-05-12 08:02:02 +0800

    [IMP] 对称的树

--- 101.对称二叉树/README.md
+++ 101.对称二叉树/README.md
@@ -1,15 +1,25 @@
-# [101. 对称二叉树]()
+# [101. 对称二叉树](https://leetcode-cn.com/problems/symmetric-tree/solution/dui-cheng-er-cha-shu-by-leetcode-solution/)
 
 `DFS` `BFS`
 
 ## 深度优先搜索
 
+对称的二叉树与 [100.相同的树](https://leetcode-cn.com/problems/same-tree/solution/xiang-tong-de-shu-by-leetcode-solution/) 不同点在于**右子树都与左子树镜像对称**
+
 ### 解题思路
+
+1. 首先判断左右子树头结点是否相同
+    - 如果都为空，对称
+    - 如果其中一个为空，另一个为非空，非对称
+    - 如果都为非空，但值不同，非对称
+2. 如果左右子树头结点都为非空，且值相同，递归遍历二叉，比较
+    - 左结点的左子结点与右结点的右子结点是否对称
+    - 左结点的右子结点与右结点的左子结点是否对称
 
 ### 复杂度
 
-- 时间复杂度：
-- 空间复杂度：
+- 时间复杂度：O(N) N 为二叉树的结点数
+- 空间复杂度：O(N) 递归调用栈深度不超过 N
 
 ## 广度优先搜索
 
--- 101.对称二叉树/solution.py
+++ 101.对称二叉树/solution.py
@@ -6,3 +6,36 @@
 # datetime    : 2021/5/11 20:33
 # filename    : solution.py
 # description : LC 101 对称二叉树
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        """ DFS """
+        if not root:
+            return True
+
+        def dfs(left: TreeNode, right: TreeNode) -> bool:
+            # 当左右结点都为空，则对称
+            if not left and not right:
+                return True
+            # 当左右结点其中一个为空，另一个不为空，则不对称
+            elif not left or not right:
+                return False
+            # 当左右结点非空，但值不同，则不对称
+            elif left.val != right.val:
+                return False
+            # 将左结点的左子结点与右结点的右子结点比较
+            # 将左结点的右子结点与右结点的左子结点比较
+            return dfs(left.left, right.right) and dfs(left.right, right.left)
+
+        return dfs(root, root)
+
+    def isSymmetricBFS(self, root: TreeNode) -> bool:
+        """ BFS """
+        pass
