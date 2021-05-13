3968191b39506d57cbea35ef86fc2f0106a459ad
Tao Qitian <taoqt@mail2.sysu.edu.cn>
2021-05-13 20:50:01 +0800

    [IMP] 将有序数组转换为二叉搜索树 DFS

--- 108.将有序数组转换为二叉搜索树/README.md
+++ 108.将有序数组转换为二叉搜索树/README.md
@@ -1,6 +1,8 @@
-# [108. 将有序数组转换为二叉搜索树]()
+# [108. 将有序数组转换为二叉搜索树](https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/solution/jian-dan-di-gui-bi-xu-miao-dong-by-sweetiee/)
 
-##
+`DFS`
+
+## 深度优先遍历
 
 ### 解题思路
 
--- 108.将有序数组转换为二叉搜索树/solution.py
+++ 108.将有序数组转换为二叉搜索树/solution.py
@@ -6,3 +6,29 @@
 # datetime    : 2021/5/12 22:18
 # filename    : solution.py
 # description : LC 108 将有序数组转换为二叉搜索树
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        """ DFS """
+        def dfs(left, right):
+            # 当 left 下标大于 right 下标，说明数组已经遍历完毕，直接返回空结点
+            if left > right:
+                return None
+            # 取中间靠左的数字作为根结点
+            # 保证每个结点的左右两个子树的高度差的绝对值不超过 1
+            mid = (left + right) // 2
+            # 模拟中序遍历
+            root = TreeNode(val=nums[mid])
+            root.left = dfs(left, mid - 1)
+            root.right = dfs(mid + 1, right)
+            return root
+        return dfs(0, len(nums) - 1)
