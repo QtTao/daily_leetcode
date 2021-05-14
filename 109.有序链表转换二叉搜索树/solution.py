0c8689b01f0602003653cfa8991ab45c4f0f64c8
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-05-14 08:00:43 +0800

    [ADD] 2021-05-10 每日一题 有序链表转换二叉搜索树

---
+++ 109.有序链表转换二叉搜索树/solution.py
@@ -0,0 +1,27 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# author      : Tao Qitian
+# email       : taoqt@mail2.sysu.edu.cn
+# datetime    : 2021/5/14 00:23
+# filename    : solution.py
+# description : LC 109 有序链表转换二叉搜索树
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        pass
