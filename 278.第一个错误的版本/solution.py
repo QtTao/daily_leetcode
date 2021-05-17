43a2e8926cd88238641b0064fb826dfd8eb78281
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-05-18 07:58:22 +0800

    [ADD] 2021-05-18 每日一题 第一个错误的版本

---
+++ 278.第一个错误的版本/solution.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# author      : Tao Qitian
+# email       : taoqt@mail2.sysu.edu.cn
+# datetime    : 2021/5/18 01:10
+# filename    : solution.py
+# description : LC 278 第一个错误的版本
