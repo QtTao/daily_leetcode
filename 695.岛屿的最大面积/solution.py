3a1d4e2792fc0f29018d1a9a2f9cdcb4e137dbb1
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-05-25 11:29:57 +0800

    [IMP] 岛屿的最大面积

--- 695.岛屿的最大面积/solution.py
+++ 695.岛屿的最大面积/solution.py
@@ -41,3 +41,40 @@ class Solution:
                 if area > max_area:
                     max_area = area
         return max_area
+
+    def maxAreaOfIslandBFS(self, grid: List[List[int]]) -> int:
+        """ BFS """
+        m, n = len(grid), len(grid[0])
+
+        def bfs(i: int, j: int):
+            """ 广度优先搜索 """
+            area = 0
+            queue = [(i, j)]
+            while queue:
+                x, y = queue.pop(0)
+                # 当遇到未访问的陆地时，面积 + 1，并标记为 2，表示已访问
+                if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                    grid[x][y] = 2
+                    area += 1
+                    # 将该陆地为中心的上下左右的位置加入队列中，等待搜索
+                    queue.extend([
+                        # 上方格子
+                        (x - 1, y),
+                        # 下方格子
+                        (x + 1, y),
+                        # 左边格子
+                        (x, y - 1),
+                        # 右边格子
+                        (x, y + 1)
+                    ])
+            return area
+
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    # 广度优先搜索，计算岛屿面积
+                    area = bfs(i, j)
+                    # 更新当前岛屿的最大面积
+                    max_area = max(area, max_area)
+        return max_area
