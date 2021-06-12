5cecd2f5e4b97374b0ad892b8b3c848406433c19
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-06-12 13:41:44 +0800

    [IMP] 课程表IV

--- 1462.课程表IV/README.md
+++ 1462.课程表IV/README.md
@@ -1,21 +1,22 @@
-# [1462. 课程表IV]()
+# [1462. 课程表IV](https://leetcode-cn.com/problems/course-schedule-iv/solution/an-ti-shi-zuo-by-abear-2-rvm7/)
 
-`BFS` `DFS`
+`Floyd`
 
-## BFS
+## 搜索
 
-### 解题思路
-
-### 复杂度
-
-- 时间复杂度：
-- 空间复杂度：
-
-## DFS
+此题可以采用 `Floyd` 算法来解决，具体实现方法可参考[这里](https://leetcode-cn.com/problems/course-schedule-iv/solution/pythonshuang-bai-floyd-warshall-transitive-closure/)
 
 ### 解题思路
 
+1. 初始化 `is_reachable` 记录课程之间是否有通路，默认值为 False
+2. 遍历 prerequisites 所有课程依赖关系，如果课程 i 是课程 j 的先修课程，那么设 `is_reachable[i][j] = True`
+3. 在内层中遍历所有课程，找出与课程 i 和课程 j 有关的所有课程，有以下两种情况
+    - 如果当前课程 k 是课程 i 的先修课程，即 `is_reachable[k][i] = True`，那么课程 k 同样是课程 j 的先修课程，设 `is_reachable[k][j] = True`
+    - 如果课程 j 是当前课程 k 的先修课程，即 `is_reachable[j][k] = True`，那么课程 i 同样是课程 k 的先修课程，设 `is_reachable[i[k] = True`
+4. 完成 prerequisites 的遍历后，得到记录任意两个课程之间是否有通路的二维数组 `is_reachable`
+5. 基于二维数组 `is_reachable`，判断 `queries` 中的依赖关系是否存在
+
 ### 复杂度
 
-- 时间复杂度：
-- 空间复杂度：
+- 时间复杂度：O(N * M) 其中 N 为 prerequisites 的长度，而 M 是课程数量
+- 空间复杂度：O(M^2) 需要 M^2 的空间来记录任意两个课程之间是否有通路
--- 1462.课程表IV/solution.py
+++ 1462.课程表IV/solution.py
@@ -5,5 +5,24 @@
 # email       : taoqt@mail2.sysu.edu.cn
 # datetime    : 2021/6/8 00:11
 # filename    : solution.py
-# description : LC 1462 课程表IV
+# description : LC 1462 课程表IV（可采用 Floyd 算法）
 
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        is_reachable = [[False for _ in range(numCourses)] for _ in range(numCourses)]
+        for first, second in prerequisites:
+            # 先修 first 课程，才能修 second 课程
+            is_reachable[first][second] = True
+            # 遍历所有课程，找出与 first 和 second 的所有依赖关系
+            for course in range(numCourses):
+                # 如果 first 课程依赖当前课程，即 current -> first，结合 first -> second，得到 current -> second
+                if is_reachable[course][first]:
+                    # 所以从当前课程可以到达 second 课程
+                    is_reachable[course][second] = True
+                # 如果当前课程依赖 second 课程，即 second -> second，结合 first -> second，得到 first -> current
+                if is_reachable[second][course]:
+                    # 所以从 first 课程可以到达当前课程
+                    is_reachable[first][course] = True
+        # 根据 prerequisites 找出所有课程通路后，再对 queries 中关系进行判断
+        return [is_reachable[first][second] for first, second in queries]
