dfec334021fc4e8fab2259c2bd99da4be45377c5
taoqitian <taoqt@mail2.sysu.edu.cn>
2021-06-21 00:02:23 +0800

    [ADD] 2021-06-18 每日一题 最长公共前缀

---
+++ 14.最长公共前缀/solution.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# author      : Tao Qitian
+# email       : taoqt@mail2.sysu.edu.cn
+# datetime    : 2021/6/20 23:08
+# filename    : solution.py
+# description : LC 14 最长公共前缀
