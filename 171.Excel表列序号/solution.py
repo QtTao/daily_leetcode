52756b30e9d51794591aca030bc918e707f473f1
Tao Qitian <taoqt@mail2.sysu.edu.cn>
2021-06-28 19:18:05 +0800

    [ADD] 2021-06-22 每日一题 Excel表列序号

---
+++ 171.Excel表列序号/solution.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# author      : Tao Qitian
+# email       : taoqt@mail2.sysu.edu.cn
+# datetime    : 2021/6/28 13:03
+# filename    : solution.py
+# description : LC 171 Excel表列序号
